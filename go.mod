837321de5d1d3f330a7e4b8b3583a7cc9b122ef9
kiper01 <kiper0328@mail.ru>
2024-01-21 03:29:51 +1000

    Утомился

--- go.mod
+++ go.mod
@@ -15,6 +15,8 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgtype v1.14.0 // indirect
 	github.com/jackc/puddle v1.3.0 // indirect
+	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
+	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
@@ -27,7 +29,7 @@ require (
 	github.com/subosito/gotenv v1.6.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
-	golang.org/x/crypto v0.16.0 // indirect
+	golang.org/x/crypto v0.17.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
@@ -35,9 +37,12 @@ require (
 
 require (
 	github.com/IAmFutureHokage/HL-Coder v0.0.0-20231203144942-15a4b87bdb31 // indirect
-	github.com/davecgh/go-spew v1.1.1
+	github.com/Masterminds/squirrel v1.5.4
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc
+	github.com/doug-martin/goqu/v9 v9.19.0
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/jackc/pgx/v4 v4.18.1
+	github.com/jackc/pgx/v5 v5.5.2
 	github.com/spf13/viper v1.18.2
 	golang.org/x/net v0.19.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
--- internal/app/repository/repository.go
+++ internal/app/repository/repository.go
@@ -1,8 +1,272 @@
 package repository
 
-// import (
-// 	"context"
-// 	"fmt"
+import (
+	"context"
+	"fmt"
 
-// 	"github.com/IAmFutureHokage/HL-BufferService/internal/app/model"
-// )
+	"github.com/IAmFutureHokage/HL-BufferService/internal/app/model"
+	"github.com/doug-martin/goqu/v9"
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
+type HydrologyBufferRepository struct {
+	dbPool *pgxpool.Pool
+}
+
+func NewHydrologyBufferRepository(pool *pgxpool.Pool) *HydrologyBufferRepository {
+	return &HydrologyBufferRepository{dbPool: pool}
+}
+
+func (r *HydrologyBufferRepository) AddTelegram(ctx context.Context, data []model.Telegram) error {
+
+	tx, err := r.dbPool.Begin(ctx)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err != nil {
+			tx.Rollback(ctx)
+			fmt.Errorf("rollback erorr")
+			return
+		}
+		err = tx.Commit(ctx)
+		if err != nil {
+			fmt.Errorf("commit error")
+		}
+	}()
+
+	for _, telegram := range data {
+		telegramInsert := goqu.Insert("telegram").Rows(
+			goqu.Record{
+				"id":                         telegram.Id,
+				"groupId":                    telegram.GroupId,
+				"telegramCode":               telegram.TelegramCode,
+				"postCode":                   telegram.PostCode,
+				"dateTime":                   telegram.DateTime,
+				"endBlockNum":                telegram.EndBlockNum,
+				"isDangerous":                telegram.IsDangerous,
+				"waterLevelOnTime":           telegram.WaterLevelOnTime,
+				"deltaWaterLevel":            telegram.DeltaWaterLevel,
+				"waterLevelOn20h":            telegram.WaterLevelOn20h,
+				"waterTemperature":           telegram.WaterTemperature,
+				"airTemperature":             telegram.AirTemperature,
+				"icePhenomeniaState":         telegram.IcePhenomeniaState,
+				"ice":                        telegram.Ice,
+				"snow":                       telegram.Snow,
+				"waterflow":                  telegram.Waterflow,
+				"precipitationValue":         telegram.PrecipitationValue,
+				"precipitationDuration":      telegram.PrecipitationDuration,
+				"reservoirDate":              telegram.ReservoirDate,
+				"headwaterLevel":             telegram.HeadwaterLevel,
+				"averageReservoirLevel":      telegram.AverageReservoirLevel,
+				"downstreamLevel":            telegram.DownstreamLevel,
+				"reservoirVolume":            telegram.ReservoirVolume,
+				"isReservoirWaterInflowDate": telegram.IsReservoirWaterInflowDate,
+				"inflow":                     telegram.Inflow,
+				"reset":                      telegram.Reset,
+			},
+		)
+
+		sql, args, err := telegramInsert.ToSQL()
+		if err != nil {
+			return err
+		}
+
+		_, err = tx.Exec(ctx, sql, args...)
+		if err != nil {
+			return err
+		}
+
+		for _, phenomen := range telegram.IcePhenomenia {
+			phenomeniaInsert := goqu.Insert("phenomenia").Rows(
+				goqu.Record{
+					"id":          phenomen.Id,
+					"telegramId":  phenomen.TelegramId,
+					"phenomen":    phenomen.Phenomen,
+					"isUntensity": phenomen.IsUntensity,
+					"intensity":   phenomen.Intensity,
+				},
+			)
+
+			sql, args, err := phenomeniaInsert.ToSQL()
+			if err != nil {
+				return err
+			}
+
+			_, err = tx.Exec(ctx, sql, args...)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func (r *HydrologyBufferRepository) GetTelegramByID(ctx context.Context, id uuid.UUID) (model.Telegram, error) {
+
+	selectTelegramBuilder := goqu.From("telegram").Where(goqu.Ex{"id": id}).Limit(1)
+	sql, args, err := selectTelegramBuilder.ToSQL()
+	if err != nil {
+		return model.Telegram{}, err
+	}
+
+	row := r.dbPool.QueryRow(ctx, sql, args...)
+
+	var telegram model.Telegram
+	err = row.Scan(
+		&telegram.Id,
+		&telegram.GroupId,
+		&telegram.TelegramCode,
+		&telegram.PostCode,
+		&telegram.DateTime,
+		&telegram.EndBlockNum,
+		&telegram.IsDangerous,
+		&telegram.WaterLevelOnTime,
+		&telegram.DeltaWaterLevel,
+		&telegram.WaterLevelOn20h,
+		&telegram.WaterTemperature,
+		&telegram.AirTemperature,
+		&telegram.IcePhenomeniaState,
+		&telegram.Ice,
+		&telegram.Snow,
+		&telegram.Waterflow,
+		&telegram.PrecipitationValue,
+		&telegram.PrecipitationDuration,
+		&telegram.ReservoirDate,
+		&telegram.HeadwaterLevel,
+		&telegram.AverageReservoirLevel,
+		&telegram.DownstreamLevel,
+		&telegram.ReservoirVolume,
+		&telegram.IsReservoirWaterInflowDate,
+		&telegram.Inflow,
+		&telegram.Reset,
+	)
+	if err != nil {
+		return model.Telegram{}, err
+	}
+
+	selectPhenomeniaBuilder := goqu.From("phenomenia").Where(goqu.Ex{"telegramId": id})
+	sql, args, err = selectPhenomeniaBuilder.ToSQL()
+	if err != nil {
+		return model.Telegram{}, err
+	}
+
+	rows, err := r.dbPool.Query(ctx, sql, args...)
+	if err != nil {
+		return model.Telegram{}, err
+	}
+	defer rows.Close()
+
+	var phenomeniaSlice []model.Phenomenia
+	var phenomeniaCount int
+
+	for rows.Next() {
+		var phenomen model.Phenomenia
+		err := rows.Scan(
+			&phenomen.Id, &phenomen.TelegramId, &phenomen.Phenomen, &phenomen.IsUntensity, &phenomen.Intensity,
+		)
+		if err != nil {
+			return model.Telegram{}, err
+		}
+		phenomeniaSlice = append(phenomeniaSlice, phenomen)
+		phenomeniaCount++
+	}
+
+	phenomeniaArray := make([]*model.Phenomenia, phenomeniaCount)
+	for i, p := range phenomeniaSlice {
+		phenomeniaArray[i] = &p
+	}
+
+	telegram.IcePhenomenia = phenomeniaArray
+
+	return telegram, nil
+}
+
+func (r *HydrologyBufferRepository) RemoveTelegram(ctx context.Context, id uuid.UUID) error {
+
+	tx, err := r.dbPool.Begin(ctx)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err != nil {
+			tx.Rollback(ctx)
+			fmt.Errorf("rollback error")
+			return
+		}
+		err = tx.Commit(ctx)
+		if err != nil {
+			fmt.Errorf("commit error")
+		}
+	}()
+
+	_, err = tx.Exec(ctx, "DELETE FROM phenomenia WHERE telegramId = $1", id)
+	if err != nil {
+		return err
+	}
+
+	_, err = tx.Exec(ctx, "DELETE FROM telegram WHERE id = $1", id)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *HydrologyBufferRepository) GetAll(ctx context.Context) ([]model.Telegram, error) {
+
+	selectBuilder := goqu.From("telegram")
+
+	sql, args, err := selectBuilder.ToSQL()
+	if err != nil {
+		return nil, err
+	}
+
+	rows, err := r.dbPool.Query(ctx, sql, args...)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var telegrams []model.Telegram
+
+	for rows.Next() {
+		var telegram model.Telegram
+		err := rows.Scan(
+			&telegram.Id,
+			&telegram.GroupId,
+			&telegram.TelegramCode,
+			&telegram.PostCode,
+			&telegram.DateTime,
+			&telegram.EndBlockNum,
+			&telegram.IsDangerous,
+			&telegram.WaterLevelOnTime,
+			&telegram.DeltaWaterLevel,
+			&telegram.WaterLevelOn20h,
+			&telegram.WaterTemperature,
+			&telegram.AirTemperature,
+			&telegram.IcePhenomeniaState,
+			&telegram.Ice,
+			&telegram.Snow,
+			&telegram.Waterflow,
+			&telegram.PrecipitationValue,
+			&telegram.PrecipitationDuration,
+			&telegram.ReservoirDate,
+			&telegram.HeadwaterLevel,
+			&telegram.AverageReservoirLevel,
+			&telegram.DownstreamLevel,
+			&telegram.ReservoirVolume,
+			&telegram.IsReservoirWaterInflowDate,
+			&telegram.Inflow,
+			&telegram.Reset,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		telegrams = append(telegrams, telegram)
+	}
+
+	return telegrams, nil
+}
