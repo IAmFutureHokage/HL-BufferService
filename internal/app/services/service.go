256bf04af1212772aff460ef8522b57e2fc85224
IAmFutureHokage <doroshenkosa@outlook.com>
2024-01-21 14:53:35 +1000

    AddTelegrams

--- internal/app/services/service.go
+++ internal/app/services/service.go
@@ -9,6 +9,7 @@ import (
 	"github.com/IAmFutureHokage/HL-BufferService/pkg/encoder"
 	"github.com/google/uuid"
 	"google.golang.org/protobuf/types/known/timestamppb"
+	"google.golang.org/protobuf/types/known/wrapperspb"
 )
 
 type HydrologyBufferervice struct {
@@ -67,58 +68,90 @@ func telegramToProto(req *model.Telegram) (res *pb.Telegram) {
 	res.IsDangerous = req.IsDangerous
 
 	if req.WaterLevelOnTime.Valid {
-		res.WaterLevelOnTime.Value = req.WaterLevelOnTime.Int32
+		res.WaterLevelOnTime = &wrapperspb.Int32Value{
+			Value: req.WaterLevelOnTime.Int32,
+		}
 	}
 	if req.DeltaWaterLevel.Valid {
-		res.DeltaWaterLevel.Value = req.DeltaWaterLevel.Int32
+		res.DeltaWaterLevel = &wrapperspb.Int32Value{
+			Value: req.DeltaWaterLevel.Int32,
+		}
 	}
 	if req.WaterLevelOn20h.Valid {
-		res.WaterLevelOn20H.Value = req.WaterLevelOn20h.Int32
+		res.WaterLevelOn20H = &wrapperspb.Int32Value{
+			Value: req.WaterLevelOn20h.Int32,
+		}
 	}
 	if req.WaterTemperature.Valid {
-		res.WaterTemperature.Value = float32(req.WaterTemperature.Float64)
+		res.WaterTemperature = &wrapperspb.DoubleValue{
+			Value: req.WaterTemperature.Float64,
+		}
 	}
 	if req.AirTemperature.Valid {
-		res.AirTemperature.Value = req.AirTemperature.Int32
+		res.AirTemperature = &wrapperspb.Int32Value{
+			Value: req.AirTemperature.Int32,
+		}
 	}
 	if req.IcePhenomeniaState.Valid {
-		res.IcePhenomeniaState.Value = int32(req.IcePhenomeniaState.Byte)
+		res.IcePhenomeniaState = &wrapperspb.Int32Value{
+			Value: int32(req.IcePhenomeniaState.Byte),
+		}
 	}
 	if req.Ice.Valid {
-		res.IceHeight.Value = req.Ice.Int32
+		res.IceHeight = &wrapperspb.Int32Value{
+			Value: req.Ice.Int32,
+		}
 	}
 	if req.Snow.Valid {
-		res.SnowHeight.Value = int32(req.Snow.Byte)
+		res.SnowHeight = &wrapperspb.Int32Value{
+			Value: int32(req.Snow.Byte),
+		}
 	}
 	if req.Waterflow.Valid {
-		res.WaterFlow.Value = float32(req.Waterflow.Float64)
+		res.WaterFlow = &wrapperspb.DoubleValue{
+			Value: req.Waterflow.Float64,
+		}
 	}
 	if req.PrecipitationValue.Valid {
-		res.PrecipitationValue.Value = float32(req.PrecipitationValue.Float64)
+		res.PrecipitationValue = &wrapperspb.DoubleValue{
+			Value: req.PrecipitationValue.Float64,
+		}
 	}
 	if req.PrecipitationDuration.Valid {
-		res.PrecipitationDuration.Value = int32(req.PrecipitationDuration.Byte)
+		res.PrecipitationDuration = &wrapperspb.Int32Value{
+			Value: int32(req.PrecipitationDuration.Byte),
+		}
 	}
 	if req.ReservoirDate.Valid {
 		res.ReservoirDate = timestamppb.New(req.ReservoirDate.Time)
 	}
 	if req.HeadwaterLevel.Valid {
-		res.HeadwaterLevel.Value = req.HeadwaterLevel.Int32
+		res.HeadwaterLevel = &wrapperspb.Int32Value{
+			Value: req.HeadwaterLevel.Int32,
+		}
 	}
 	if req.AverageReservoirLevel.Valid {
-		res.AverageReservoirLevel.Value = req.AverageReservoirLevel.Int32
+		res.AverageReservoirLevel = &wrapperspb.Int32Value{
+			Value: req.AverageReservoirLevel.Int32,
+		}
 	}
 	if req.DownstreamLevel.Valid {
-		res.DownstreamLevel.Value = req.DownstreamLevel.Int32
+		res.DownstreamLevel = &wrapperspb.Int32Value{
+			Value: req.DownstreamLevel.Int32,
+		}
 	}
 	if req.ReservoirVolume.Valid {
-		res.ReservoirVolume.Value = float32(req.ReservoirVolume.Float64)
+		res.ReservoirVolume = &wrapperspb.DoubleValue{
+			Value: req.ReservoirVolume.Float64,
+		}
 	}
 	if req.IsReservoirWaterInflowDate.Valid {
 		res.ReservoirWaterInflowDate = timestamppb.New(req.IsReservoirWaterInflowDate.Time)
 	}
 	if req.Reset.Valid {
-		res.Reset_.Value = float32(req.Reset.Float64)
+		res.Reset_ = &wrapperspb.DoubleValue{
+			Value: req.Reset.Float64,
+		}
 	}
 
 	if len(req.IcePhenomenia) != 0 {
@@ -126,8 +159,11 @@ func telegramToProto(req *model.Telegram) (res *pb.Telegram) {
 
 		for i := 0; i < len(res.IcePhenomenias); i++ {
 			res.IcePhenomenias[i] = &pb.IcePhenomenia{Phenomen: int32(req.IcePhenomenia[i].Phenomen)}
+
 			if req.IcePhenomenia[i].IsUntensity && req.IcePhenomenia[i].Intensity.Valid {
-				res.IcePhenomenias[i].Intensity.Value = int32(req.IcePhenomenia[i].Intensity.Byte)
+				res.IcePhenomenias[i].Intensity = &wrapperspb.Int32Value{
+					Value: int32(req.IcePhenomenia[i].Intensity.Byte),
+				}
 			}
 		}
 	}
